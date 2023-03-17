c6cd9c40236059c93f5decb6426616f5a7d06fdf
unknown <annahrishko@ukr.net>
2023-03-17 19:50:39 +0200

    правки desctop

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,11 @@
 
 
  $('.content').hide().eq(0).show();
-  $('.name_sec').click(function() {
-    $('.name_sec').removeClass('active')
+  $('.accordion_sec').click(function() {
+    $('.accordion_sec').removeClass('active')
     $(this).addClass('active')
     $(".content").hide();
-    $(this).next().slideDown();
+    $(this).find('.content').slideDown();
     return false;
   });
 
@@ -73,6 +73,20 @@ $(function () {
 });
 
 $(function () {
+  $('.conection_with_us').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true
+  });
+  $(document).on('click', '.close_popup', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+});
+
+
+$(function () {
   $('.detali_link').magnificPopup({
     type: 'inline',
     preloader: false,
@@ -83,4 +97,73 @@ $(function () {
     e.preventDefault();
     $.magnificPopup.close();
   });
+});
+
+$(function() {
+    $.validator.addMethod("emailRegex", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i.test(value);
+    }, "Недійсний формат входу");
+});
+
+jQuery(function($){
+   
+   $(".phoneInput").mask("+ 38 (999) 999-99-99");
+});
+
+$(function() {
+    $(".form_connection").validate({
+        rules: {
+            "first_name": {
+                required: true,
+                minlength: 3,
+            },
+            "phone": {
+                required: true,
+            },
+            "email":{
+                required: true,
+                emailRegex: true,
+            },
+
+        },
+        messages: {
+            "email": {
+                required: "Ви повинні ввести ім'я електронної пошти",
+                emailRegex: "Недійсний формат входу",
+            },
+            "first_name": {
+                required: "Обов'язковие поле для заповнювання",
+                minlength: "Введіть Ваше повне ім'я",
+            },
+            "phone": {
+                required: "Обов'язковие поле для заповнювання",
+
+            },
+        },
+    });
+});
+
+$(function() {
+    $(".form_call").validate({
+        rules: {
+            "first_name": {
+                required: true,
+                minlength: 3,
+            },
+            "phone": {
+                required: true,
+            },
+
+        },
+        messages: {
+            "first_name": {
+                required: "Обов'язковие поле для заповнювання",
+                minlength: "Введіть Ваше повне ім'я",
+            },
+            "phone": {
+                required: "Обов'язковие поле для заповнювання",
+
+            },
+        },
+    });
 });
